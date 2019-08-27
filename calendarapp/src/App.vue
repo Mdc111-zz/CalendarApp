c7ea47bed5640776ea648fbe9d6e4ff326c438c3
Matthew <matt_cooper1@hotmail.com>
2019-08-27 16:38:39 +0100

    setup basic app with needed tabs

--- calendarapp/src/App.vue
+++ calendarapp/src/App.vue
@@ -1,17 +1,65 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <b-tabs content-class="mt-3">
+      <b-tab title="Week" active>
+        <b-tabs content-class="mt-3">
+          <b-tab title="Helipad One">
+ 
+          </b-tab>
+          <b-tab title="Helipad Two">
+          
+          </b-tab>
+          <b-tab title="Helipad Three">
+          
+          </b-tab>
+          <b-tab title="Helipad Four">
+          
+          </b-tab>
+        </b-tabs>
+      </b-tab>
+      <b-tab title="Day">
+        
+      </b-tab>
+    </b-tabs>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FullCalendar from '@fullcalendar/vue'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+
+import '@fullcalendar/core/main.css'
+import '@fullcalendar/daygrid/main.css'
+import '@fullcalendar/timegrid/main.css'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+ components: {
+    FullCalendar
+  },
+  data: function() {
+    return {
+      calendarPlugins: [
+        dayGridPlugin,
+        timeGridPlugin,
+        interactionPlugin
+      ],
+      calendarEvents: [
+        { title: 'Event Now', start: new Date() }
+      ]
+    }
+  },
+  methods: {
+    handleDateClick(arg) {
+      if (confirm('Would you like to add an event to ' + arg.dateStr + ' ?')) {
+        this.calendarEvents.push({
+          title: 'New Event',
+          start: arg.date,
+          allDay: arg.allDay
+        })
+      }
+    }
   }
 }
 </script>
@@ -24,5 +72,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.demo-app {
+  font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+  font-size: 14px;
+}
+.demo-app-top {
+  margin: 0 0 3em;
+}
+.demo-app-calendar {
+  margin: 0 auto;
+  max-width: 900px;
+}
+.demo-app-day-calendar{
+  margin: 0 auto;
+  max-width: 200px;
 }
 </style>
