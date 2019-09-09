ab99a8a60c2aadc0d96faa635b24ac10f4041587
Matthew <matt_cooper1@hotmail.com>
2019-09-09 12:03:02 +0100

    moved helipad logic out of daily calendar and dayclick into its own file

--- js/DailyCalendar.js
+++ js/DailyCalendar.js
@@ -16,44 +16,13 @@ $(document).ready(function() {
         },
         allDaySlot: false,
         defaultView: 'multiColAgendaDay',
-        defaultDate: '2015-02-12',
         editable: true,
         events: eventArr1,
         eventClick: function(info){
             alert("event clicked");
         },
         dayClick: function(date, jsEvent, view){
-            if(document.getElementById("myForm").style.display != "block")
-                openForm();
-
-            var dateAndTimeArray = date.format().toString().split("T");
-            var dateClicked = dateAndTimeArray[0];
-            var timeClicked = dateAndTimeArray[1];
-
-            $("input[name=scheduledate]").val(dateClicked);
-            $("input[name=starttime]").val(timeClicked);
+            handleDayClickEvent(date);
         },
     });
 });
-
-
-//$(window).click(function(event) {
-    //if (!event.target.matches('.dropbtn')) {
-    //    document.getElementsByClassName(".dropbtn").style.display = "none";
-    //}
-//});
-
-function showHelipadDropdown(){
-    document.getElementById("myDropdown").style.display = "block";
-}
-
-function toggleDisplay() {
-    if(document.getElementById("myDropdown").style.display == "block")
-      document.getElementById("myDropdown").style.display = "none";
-    else
-      document.getElementById("myDropdown").style.display = "block";
-}
-  
-function updateCurrentHelipadText(currentlySelectedItem){
-    document.getElementById("testid").innerHTML = currentlySelectedItem;
-}
