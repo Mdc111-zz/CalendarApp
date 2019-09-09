e34267d90301f147913272aee82f84044d8cbbac
Matthew <matt_cooper1@hotmail.com>
2019-09-09 09:26:17 +0100

    refactored event data and event form logic out of this file

--- js/DailyCalendar.js
+++ js/DailyCalendar.js
@@ -32,56 +32,15 @@ $(document).ready(function() {
 
             $("input[name=scheduledate]").val(dateClicked);
             $("input[name=starttime]").val(timeClicked);
-            $("input[name=helipadnumber]").val("testvalue4");
         },
     });
 });
 
-function toggleForm() {
-    //$("#dayviewcalendar").fullCalendar('removeEvents');
-    //$("#dayviewcalendar").fullCalendar('addEventSource', eventArr2);
-    if(document.getElementById("myForm").style.display == "block")
-        closeForm();
-    else
-        openForm();
-  }
-  
-function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-}
-
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
-}
-
-function handleDynamicSchedule(){
-    var title = $("input[name=title]").val();
-    var dateOfSchedule = $("input[name=scheduledate]").val();
-    var startTime = $("input[name=starttime]").val();
-    var endTime = $("input[name=endtime]").val();
-    var helipadNumber = $("input[name=helipadnumber]").val();
-    var description = $("input[name=description]").val();
-
-    var date = moment(dateOfSchedule);
-
-    //if (date.isValid()) {
-        $('#dayviewcalendar').fullCalendar('renderEvent', {
-            title: 'Meeting 2',
-            start: '2015-02-12T10:30:00',
-            end: '2015-02-12T12:30:00',
-            column: 3,
-            description: "description 2"
-        });
-        alert('Great. Now, update your database...');
-    //} else {
-    //    alert('Invalid date.');
-    //}
-}
 
 //$(window).click(function(event) {
-//    if (!event.target.matches('.dropbtn')) {
-//        document.getElementsByClassName(".dropbtn").style.display = "none";
-//    }
+    //if (!event.target.matches('.dropbtn')) {
+    //    document.getElementsByClassName(".dropbtn").style.display = "none";
+    //}
 //});
 
 function showHelipadDropdown(){
@@ -98,32 +57,3 @@ function toggleDisplay() {
 function updateCurrentHelipadText(currentlySelectedItem){
     document.getElementById("testid").innerHTML = currentlySelectedItem;
 }
-
-
-
-var eventArr1 = [
-    {
-        title: 'Meeting 1',
-        start: '2015-02-12T10:30:00',
-        end: '2015-02-12T12:30:00',
-        column: 0,
-        description: "description 1"
-    },
-    {
-        title: 'Meeting 2',
-        start: '2015-02-12T10:30:00',
-        end: '2015-02-12T12:30:00',
-        column: 1,
-        description: "description 2"
-    }
-];
-
-var eventArr2 = [
-    {
-        title: 'Meeting 2',
-        start: '2015-02-11T10:30:00',
-        end: '2015-02-11T12:30:00',
-        column: 1,
-        description: "description 2"
-    }
-];
