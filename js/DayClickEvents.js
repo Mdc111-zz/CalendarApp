781b0bc5a907db0905701f9b9d3df3730da1f15b
Matthew <matt_cooper1@hotmail.com>
2019-09-09 12:01:45 +0100

    Made files for handling day click events, event click events and all window events

---
+++ js/DayClickEvents.js
@@ -0,0 +1,11 @@
+function handleDayClickEvent(date){
+    if(document.getElementById("myForm").style.display != "block")
+    openForm();
+
+    var dateAndTimeArray = date.format().toString().split("T");
+    var dateClicked = dateAndTimeArray[0];
+    var timeClicked = dateAndTimeArray[1];
+
+    $("input[name=scheduledate]").val(dateClicked);
+    $("input[name=starttime]").val(timeClicked);
+}
---
+++ js/WindowEvents.js
@@ -0,0 +1,5 @@
+$(window).click(function(event) {
+    if (wasDropDownClicked()) {
+        document.getElementById("myDropdown").style.display = "none";
+    }
+});
