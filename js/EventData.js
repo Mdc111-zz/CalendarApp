dfef0a6d1e1cca8f8d96d6a9a79b562ccab51609
Matthew <matt_cooper1@hotmail.com>
2019-09-09 09:25:40 +0100

    moved all of the event data into one file

---
+++ js/EventData.js
@@ -0,0 +1,16 @@
+var eventArr1 = [
+    {
+        title: 'Meeting 1',
+        start: '2015-02-12T10:30:00',
+        end: '2015-02-12T12:30:00',
+        column: 0,
+        description: "description 1"
+    },
+    {
+        title: 'Meeting 2',
+        start: '2015-02-12T10:30:00',
+        end: '2015-02-12T12:30:00',
+        column: 1,
+        description: "description 2"
+    }
+];
