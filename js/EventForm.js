e319d7424a80ea618bb60aa2d402cd137f59a8f4
Matthew <matt_cooper1@hotmail.com>
2019-09-10 12:29:06 +0100

    manual clear form function. upon submitting event will render if needed

--- js/EventForm.js
+++ js/EventForm.js
@@ -21,14 +21,76 @@ function handleDynamicSchedule(){
     var helipadNumber = parseInt($("input[name=helipadnumber]").val());
     var description = $("input[name=description]").val();
 
-    var event = {
+    var dayEvent = {
+        id: generateEventId,
         title: title,
         start: dateOfSchedule + "T" + startTime,
         end: dateOfSchedule + "T" + endTime,
-        column: helipadNumber,
+        column: (helipadNumber - 1),
         description: description
     };
 
-    eventArr1.push(event);
-    $('#dayviewcalendar').fullCalendar('renderEvent', event);
+    var weekEvent = {
+        id: generateEventId,
+        title: title,
+        start: dateOfSchedule + "T" + startTime,
+        end: dateOfSchedule + "T" + endTime,
+        description: description
+    };
+    if(helipadNumber == 1){
+        weekEventDataHelipad1.push(weekEvent);
+    }
+    else if(helipadNumber == 2){
+        weekEventDataHelipad2.push(weekEvent);
+    }
+    else if(helipadNumber == 3){
+        weekEventDataHelipad3.push(weekEvent);
+    }
+    else if(helipadNumber == 4){
+        weekEventDataHelipad4.push(weekEvent);
+    }
+    dayEventData.push(dayEvent)
+
+    if(currentOpen == "dayview"){
+        $("#dayviewcalendar").fullCalendar('removeEvents');
+        $("#dayviewcalendar").fullCalendar('addEventSource', dayEventData);
+    }else if(currentOpen == "weekview"){
+        var currentlySelectedHelipad = document.getElementById("dropbtn").innerHTML;
+
+        if(currentlySelectedHelipad == "Select Helipad"){
+          $("#weekviewcalendar").fullCalendar('removeEvents');
+          $("#weekviewcalendar").fullCalendar('addEventSource', []);
+        }
+        else if(currentlySelectedHelipad == "Helipad 1"){
+          $("#weekviewcalendar").fullCalendar('removeEvents');
+          $("#weekviewcalendar").fullCalendar('addEventSource', weekEventDataHelipad1);
+        }
+        else if(currentlySelectedHelipad == "Helipad 2"){
+          $("#weekviewcalendar").fullCalendar('removeEvents');
+          $("#weekviewcalendar").fullCalendar('addEventSource', weekEventDataHelipad2);
+        }
+        else if(currentlySelectedHelipad == "Helipad 3"){
+          $("#weekviewcalendar").fullCalendar('removeEvents');
+          $("#weekviewcalendar").fullCalendar('addEventSource', weekEventDataHelipad3);
+        }
+        else if(currentlySelectedHelipad == "Helipad 4"){
+          $("#weekviewcalendar").fullCalendar('removeEvents');
+          $("#weekviewcalendar").fullCalendar('addEventSource', weekEventDataHelipad4);
+        }
+    }
+    closeForm();
+    clearForm();
+}
+
+function clearForm(){
+    $("input[name=title]").val("");
+    $("input[name=scheduledate]").val("");
+    $("input[name=starttime]").val("");
+    $("input[name=endtime]").val("");
+    $("input[name=helipadnumber]").val("");
+    $("input[name=description]").val("");
+}
+
+function generateEventId(){
+    return Math.floor(Math.random() * Math.floor(1000000));
 }
