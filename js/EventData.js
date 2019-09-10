a14cad4aa60346a0d83f9882e411eb3334461392
Matthew <matt_cooper1@hotmail.com>
2019-09-10 17:25:14 +0100

    updated event data

--- js/EventData.js
+++ js/EventData.js
@@ -1,16 +1,74 @@
-var eventArr1 = [
+var dayEventData = [
     {
+        id: "1",
         title: 'Meeting 1',
-        start: '2015-02-12T10:30:00',
-        end: '2015-02-12T12:30:00',
+        start: '2019-09-09T10:30:00',
+        end: '2019-09-09T12:30:00',
         column: 0,
         description: "description 1"
     },
     {
+        id: "2",
         title: 'Meeting 2',
-        start: '2015-02-12T10:30:00',
-        end: '2015-02-12T12:30:00',
+        start: '2019-09-09T10:30:00',
+        end: '2019-09-09T12:30:00',
         column: 1,
         description: "description 2"
+    },
+    {
+        id: "3",
+        title: 'Meeting 3',
+        start: '2019-09-09T10:30:00',
+        end: '2019-09-09T12:30:00',
+        column: 2,
+        description: "description 3"
+    },
+    {
+        id: "4",
+        title: 'Meeting 4',
+        start: '2019-09-09T10:30:00',
+        end: '2019-09-09T12:30:00',
+        column: 3,
+        description: "description 4"
     }
+];
+
+var weekEventDataHelipad1 = [
+    {
+        id: "1",
+        title: 'Meeting 1',
+        start: '2019-09-09T10:30:00',
+        end: '2019-09-09T12:30:00',
+        description: "description 1"
+    },
+];
+
+var weekEventDataHelipad2 = [
+    {
+        id: "2",
+        title: 'Meeting 2',
+        start: '2019-09-09T10:30:00',
+        end: '2019-09-09T12:30:00',
+        description: "description 2"
+    },
+];
+
+var weekEventDataHelipad3 = [
+    {
+        id: "3",
+        title: 'Meeting 3',
+        start: '2019-09-09T10:30:00',
+        end: '2019-09-09T12:30:00',
+        description: "description 3"
+    },
+];
+
+var weekEventDataHelipad4 = [
+    {
+        id: "4",
+        title: 'Meeting 4',
+        start: '2019-09-09T10:30:00',
+        end: '2019-09-09T12:30:00',
+        description: "description 4"
+    },
 ];
