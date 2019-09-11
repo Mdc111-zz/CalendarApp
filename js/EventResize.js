9f678cc4f90106e198639302e020b2db55cdbd2a
Matthew <matt_cooper1@hotmail.com>
2019-09-11 12:12:57 +0100

    refactored and stopped using event.end._i which returned clientevent data not our data

--- js/EventResize.js
+++ js/EventResize.js
@@ -1,36 +1,22 @@
-function handleResizeEvent(event, delta){
-    var idOfResizedEvent = event.id;
-    var helipadNumber = event.column + 1;
-    var date = event.end._i.substring(0, event.end._i.indexOf("T"));
-    var minutesResizedBy = delta._data.minutes;
-    var hoursResizedBy = delta._data.hours;
-
+function handleResizeEvent(eventId, hoursResizedBy, minutesResizedBy, helipadNumber){
     if(helipadNumber == 1){
-        var indexOfEventInArray = weekEventDataHelipad1.findIndex(x => x.id == idOfResizedEvent);
-        var newEndTime = calculateNewTime(weekEventDataHelipad1[indexOfEventInArray].end, date, minutesResizedBy, hoursResizedBy);
-        weekEventDataHelipad1[indexOfEventInArray].end = newEndTime;
-
+        var indexOfEventInArray = weekEventDataHelipad1.findIndex(x => x.id == eventId);
+        weekEventDataHelipad1[indexOfEventInArray].end = calculateAdjustedTime(weekEventDataHelipad1[indexOfEventInArray].end, 0, hoursResizedBy, minutesResizedBy);;
+        console.log(weekEventDataHelipad1[indexOfEventInArray]);
     }
     else if(helipadNumber == 2){
-        var indexOfEventInArray = weekEventDataHelipad2.findIndex(x => x.id == idOfResizedEvent);
-        var newEndTime = calculateNewTime(weekEventDataHelipad2[indexOfEventInArray].end, date, minutesResizedBy, hoursResizedBy);
-        weekEventDataHelipad2[indexOfEventInArray].end = newEndTime;
+        var indexOfEventInArray = weekEventDataHelipad2.findIndex(x => x.id == eventId);
+        weekEventDataHelipad2[indexOfEventInArray].end = calculateAdjustedTime(weekEventDataHelipad2[indexOfEventInArray].end, 0, hoursResizedBy, minutesResizedBy);;
     }
     else if(helipadNumber == 3){
-        var indexOfEventInArray = weekEventDataHelipad3.findIndex(x => x.id == idOfResizedEvent);
-        var newEndTime = calculateNewTime(weekEventDataHelipad3[indexOfEventInArray].end, date, minutesResizedBy, hoursResizedBy);
-        weekEventDataHelipad3[indexOfEventInArray].end = newEndTime;
+        var indexOfEventInArray = weekEventDataHelipad3.findIndex(x => x.id == eventId);
+        weekEventDataHelipad3[indexOfEventInArray].end = calculateAdjustedTime(weekEventDataHelipad3[indexOfEventInArray].end, 0, hoursResizedBy, minutesResizedBy);
     }
     else if(helipadNumber == 4){
-        var indexOfEventInArray = weekEventDataHelipad4.findIndex(x => x.id == idOfResizedEvent);
-        var newEndTime = calculateNewTime(weekEventDataHelipad4[indexOfEventInArray].end, date, minutesResizedBy, hoursResizedBy);
-        weekEventDataHelipad4[indexOfEventInArray].end = newEndTime;
+        var indexOfEventInArray = weekEventDataHelipad4.findIndex(x => x.id == eventId);
+        weekEventDataHelipad4[indexOfEventInArray].end = calculateAdjustedTime(weekEventDataHelipad4[indexOfEventInArray].end, 0, hoursResizedBy, minutesResizedBy);
     }
-    var indexOfEventInArray = dayEventData.findIndex(x => x.id == idOfResizedEvent);
-    var newEndTime = calculateNewTime(dayEventData[indexOfEventInArray].end, date, minutesResizedBy, hoursResizedBy);
-    dayEventData[indexOfEventInArray].end = newEndTime;
-}
-
-function calculateNewTime(oldEndTime, date, minutes, hours){
-    return "2019-09-09T14:00:00";
+    var indexOfEventInArray = dayEventData.findIndex(x => x.id == eventId);
+    dayEventData[indexOfEventInArray].end = calculateAdjustedTime(dayEventData[indexOfEventInArray].end, 0, hoursResizedBy, minutesResizedBy);
+    console.log(dayEventData[indexOfEventInArray]);
 }
