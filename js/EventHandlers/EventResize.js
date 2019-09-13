206f20bdd72c26ae0e67796596eedda4e3f46049
Matthew <matt_cooper1@hotmail.com>
2019-09-13 13:37:19 +0100

    removed passing an unneeded variable

--- js/EventHandlers/EventResize.js
+++ js/EventHandlers/EventResize.js
@@ -1,4 +1,4 @@
-function handleResizeEvent(eventId, hoursResizedBy, minutesResizedBy, helipadNumber){
+function handleResizeEvent(eventId, hoursResizedBy, minutesResizedBy){
     var indexOfEventInArray = eventData.findIndex(x => x.id == eventId);
     eventData[indexOfEventInArray].end = calculateAdjustedTime(eventData[indexOfEventInArray].end, 0, hoursResizedBy, minutesResizedBy);
 }
