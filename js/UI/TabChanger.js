a5d45524837ff53f8c70d7e6fa12bf05f8df6fc9
Matthew <matt_cooper1@hotmail.com>
2019-09-16 14:24:31 +0100

    fixed bug. Do rendering after display change

--- js/UI/TabChanger.js
+++ js/UI/TabChanger.js
@@ -15,27 +15,16 @@ function toggleTab(elementId) {
 }
 
 function changeToDayView(){
-  console.log(eventData);
-  renderCalendarData(currentOpen, eventData);
-  $("#dayviewcalendar").fullCalendar( "rerenderEvents" );
   document.getElementById("dayview").style.display = "block";
   document.getElementById("weekview").style.display = "none";
+  renderCalendarData(currentOpen, eventData);
 }
 
 function changeToWeekView(){
-  //let currentlySelectedHelipad = GetHelipadNumberFromDropDown() - 1;
-
-  //let dataToRender = currentlySelectedHelipad == 5 ? [] : filterArrayByHelipad(eventData, currentlySelectedHelipad);
-  //renderCalendarData(currentOpen, dataToRender);
-
-  document.getElementById("dropbtn").innerHTML = "Select Helipad";
-
-  //$("#weekviewcalendar").fullCalendar('removeEventSource', eventData )
-  //$(calendarType).fullCalendar('removeEvents');
-  //$("#weekviewcalendar").fullCalendar('addEventSource', dataToRender);
-  $("#weekviewcalendar").fullCalendar('refetchEvents');
-
+  let currentlySelectedHelipad = GetHelipadNumberFromDropDown() - 1;
+  let dataToRender = currentlySelectedHelipad == 5 ? [] : filterArrayByHelipad(eventData, currentlySelectedHelipad);
 
   document.getElementById("weekview").style.display = "block";
   document.getElementById("dayview").style.display = "none";
+  renderCalendarData(currentOpen, dataToRender);
 }
