4089d51f5f2ef8a0cf9ab47d77ed94c08da13675
Matthew <matt_cooper1@hotmail.com>
2019-09-16 16:56:38 +0100

    refactoring

--- js/EventHandlers/EventClickEvents.js
+++ js/EventHandlers/EventClickEvents.js
@@ -1,10 +1,7 @@
-var eventId;
-
 function handleEventClickEvent(clickedEventId){
-    openForm("DescriptionForm");
+    openDescriptionForm(clickedEventId);
 
-    eventId = clickedEventId;
-    var indexOfEventInArray = eventData.findIndex(x => x.id == eventId);
+    var indexOfEventInArray = eventData.findIndex(x => x.id == clickedEventId);
     var eventFromOurData = eventData[indexOfEventInArray];
 
     let date = eventFromOurData.start.substring(0, eventFromOurData.start.indexOf("T"));
@@ -17,31 +14,4 @@ function handleEventClickEvent(clickedEventId){
     $("input[name=endtime]").val(endTime);
     $("input[name=helipadnumber]").val(eventFromOurData.column + 1);
     $("input[name=description]").val(eventFromOurData.description);
-}
-
-function updateEvent(){
-    eventData = removeElementFromArrayById(eventData, eventId);
-
-    eventData.push({
-        id: eventId,
-        title: $("input[name=title]").val(),
-        start: $("input[name=scheduledate]").val() + "T" + $("input[name=starttime]").val(),
-        end: $("input[name=scheduledate]").val() + "T" + $("input[name=endtime]").val(),
-        column: $("input[name=helipadnumber]").val() - 1,
-        description: $("input[name=description]").val()
-    });
-
-    let dataToRender = currentOpen == "#dayviewcalendar" ? eventData : filterArrayByHelipad(eventData, GetHelipadNumberFromDropDown() - 1);
-    renderCalendarData(currentOpen, dataToRender);
-}
-
-
-function deleteEvent(){
-    let helipadNumber = eventData[eventData.findIndex(x => x.id == eventId)].column;
-    eventData = removeElementFromArrayById(eventData, eventId);
-
-    let dataToRender = currentOpen == "#dayviewcalendar" ? eventData : filterArrayByHelipad(eventData, helipadNumber);
-    renderCalendarData(currentOpen, dataToRender);
-
-    closeForm2();
 }
